b524237f13845353c7144517a02bf5711b7cd13d
navatejareddy <ntejdev@gmail.com>
2018-02-11 21:13:08 -0500

    modified view and associated logic

--- app/src/main/java/www/ntej/com/donotown/MainActivity.java
+++ app/src/main/java/www/ntej/com/donotown/MainActivity.java
@@ -14,7 +14,7 @@ import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    private EditText person1, person2, person3, person4, person5, person6, personsOwe;
+    private EditText person1, person2, person3, person5_0,person5_1,person5_2,person5_3, person6, personsOwe;
     private TextView total;
     private Button totalButton;
     private SharedPreferences sharedPrefs;
@@ -31,8 +31,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         person1 = (EditText) findViewById(R.id.person1ET);
         person2 = (EditText) findViewById(R.id.person2ET);
         person3 = (EditText) findViewById(R.id.person3ET);
-        person4 = (EditText) findViewById(R.id.person4ET);
-        person5 = (EditText) findViewById(R.id.person5ET);
+        person5_0 = (EditText) findViewById(R.id.person5_0ET);
+        person5_1 = (EditText) findViewById(R.id.person5_1ET);
+        person5_2 = (EditText) findViewById(R.id.person5_2ET);
+        person5_3 = (EditText) findViewById(R.id.person5_3ET);
         person6 = (EditText) findViewById(R.id.person6ET);
         personsOwe = (EditText) findViewById(R.id.personOweET);
         total = (TextView) findViewById(R.id.netTV);
@@ -45,8 +47,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         person1.setText(sharedPrefs.getString(getString(R.string.person1),"0"));
         person2.setText(sharedPrefs.getString(getString(R.string.person2),"0"));
         person3.setText(sharedPrefs.getString(getString(R.string.person3),"0"));
-        person4.setText(sharedPrefs.getString(getString(R.string.person4),"0"));
-        person5.setText(sharedPrefs.getString(getString(R.string.person5),"0"));
+        person5_0.setText(sharedPrefs.getString(getString(R.string.person5_0),"0"));
+        person5_1.setText(sharedPrefs.getString(getString(R.string.person5_1),"0"));
+        person5_2.setText(sharedPrefs.getString(getString(R.string.person5_2),"0"));
+        person5_3.setText(sharedPrefs.getString(getString(R.string.person5_3),"0"));
         person6.setText(sharedPrefs.getString(getString(R.string.person6),"0"));
         personsOwe.setText(sharedPrefs.getString(getString(R.string.person_owe),"0"));
     }
@@ -55,8 +59,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         int totalFromEditText = Integer.parseInt(person1.getText().toString())
                 + Integer.parseInt(person2.getText().toString())
                 + Integer.parseInt(person3.getText().toString())
-                + Integer.parseInt(person4.getText().toString())
-                + Integer.parseInt(person5.getText().toString())
+                + Integer.parseInt(person5_0.getText().toString())
+                + Integer.parseInt(person5_1.getText().toString())
+                + Integer.parseInt(person5_2.getText().toString())
+                + Integer.parseInt(person5_3.getText().toString())
                 + Integer.parseInt(person6.getText().toString())
                 - Integer.parseInt(personsOwe.getText().toString());
         Log.i(TAG, "displayFuckingTotal:" + totalFromEditText);
@@ -80,11 +86,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
         if (person3.getText().toString().isEmpty()) {
             person3.setText("0");
         }
-        if (person4.getText().toString().isEmpty()) {
-            person4.setText("0");
+        if (person5_0.getText().toString().isEmpty()) {
+            person5_0.setText("0");
         }
-        if (person5.getText().toString().isEmpty()) {
-            person5.setText("0");
+        if (person5_1.getText().toString().isEmpty()) {
+            person5_1.setText("0");
+        }
+        if (person5_2.getText().toString().isEmpty()) {
+            person5_2.setText("0");
+        }
+        if (person5_3.getText().toString().isEmpty()) {
+            person5_3.setText("0");
         }
         if (person6.getText().toString().isEmpty()) {
             person6.setText("0");
@@ -103,8 +115,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         editor.putString(getString(R.string.person1), person1.getText().toString());
         editor.putString(getString(R.string.person2), person2.getText().toString());
         editor.putString(getString(R.string.person3), person3.getText().toString());
-        editor.putString(getString(R.string.person4), person4.getText().toString());
-        editor.putString(getString(R.string.person5), person5.getText().toString());
+        editor.putString(getString(R.string.person5_0), person5_0.getText().toString());
+        editor.putString(getString(R.string.person5_1), person5_1.getText().toString());
+        editor.putString(getString(R.string.person5_2), person5_2.getText().toString());
+        editor.putString(getString(R.string.person5_3), person5_3.getText().toString());
         editor.putString(getString(R.string.person6), person6.getText().toString());
         editor.putString(getString(R.string.person_owe), personsOwe.getText().toString());
         editor.apply();
